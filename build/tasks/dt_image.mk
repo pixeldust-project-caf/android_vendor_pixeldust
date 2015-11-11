91294aa56636298c66cb9cafd9bba9230453bc85
Nick Reuter <nreuter85@gmail.com>
2015-11-11 15:03:21 -0600

    dt_image: allow TARGET_KERNEL_ARCH to be set
     * fall back to TARGET_ARCH if not set
    
    Change-Id: Iee8cf2da0446941584ed0ab4744e78a0cfe865d1
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/tasks/dt_image.mk
+++ build/tasks/dt_image.mk
@@ -14,17 +14,18 @@ else
 DTBTOOL_NAME := $(TARGET_CUSTOM_DTBTOOL)
 endif
 
+ifneq ($(TARGET_KERNEL_ARCH),)
+KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
+else
+KERNEL_ARCH := $(TARGET_ARCH)
+endif
+
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/$(DTBTOOL_NAME)$(HOST_EXECUTABLE_SUFFIX)
 
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
-ifeq ($(strip $(TARGET_CUSTOM_DTBTOOL)),)
-# dtbToolLineage will search subdirectories
-possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-else
-# Most specific paths must come first in possible_dtb_dirs
 possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-endif
+dtb_dir = $(firstword $(wildcard $(possible_dtb_dirs)))
 
 define build-dtimage-target
     $(call pretty,"Target dt image: $@")
@@ -56,7 +57,6 @@ ALL_MODULES.$(LOCAL_MODULE).INSTALLED += $(INSTALLED_DTIMAGE_TARGET)
 .PHONY: dtimage
 dtimage: $(INSTALLED_DTIMAGE_TARGET)
 
-endif
 endif
 endif
 endif
