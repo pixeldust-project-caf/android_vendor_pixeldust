ba2962edc5569a2c409dc6a993f8004d2e185c93
luca020400 <luca.stefani.ge1@gmail.com>
2016-05-09 15:16:19 +0200

    ninja: build: dt: Move to in-build directory search
    * We can't depend on a $OUT variable with ninja
    
    Change-Id: I9ff050a986e7bb2774d1326f5b396d0afb7f33c1
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/tasks/dt_image.mk
+++ build/tasks/dt_image.mk
@@ -25,7 +25,6 @@ DTBTOOL := $(HOST_OUT_EXECUTABLES)/$(DTBTOOL_NAME)$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
 
 possible_dtb_dirs = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/
-dtb_dir = $(firstword $(wildcard $(possible_dtb_dirs)))
 
 define build-dtimage-target
     $(call pretty,"Target dt image: $@")
