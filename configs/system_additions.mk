10b4b2e302c3f8c134ceea1e1a00d62c7aa3a857
Alex Naidis <alex.naidis@linux.com>
2016-11-12 22:18:52 +0100

    SDCLANG: print warning for non-linux hosts
    SDCLANG is not supported on non-linux hosts,
    move the blocking logic from build system
    to device/qcom/common.
    
    Change-Id: I2a1f69e5b1dde05c873683ca7e6b7425c79df848
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>
    Signed-off-by: spezi77 <spezi77@gmx.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -17,5 +17,9 @@ ifeq ($(HOST_OS),linux)
     ifneq ($(wildcard vendor/qcom/sdclang-4.0/),)
         include vendor/pixeldust/sdclang/sdclang.mk
     endif
+else
+$(warning ****************************************************************)
+$(warning * SDCLANG is not supported on non-linux hosts. Disabling...)
+$(warning ****************************************************************)
 endif
 
