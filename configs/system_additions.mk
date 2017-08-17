1654ad90f0bc309246fcf175f5ca60bdb6463d65
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:43:22 +0200

    products: Disable vendor restrictions
    Change-Id: I9bd368f04605df469f02b26dae47e587bccc2e52
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Disable vendor restrictions
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
 # Include explicitly to work around GMS issues
 PRODUCT_PACKAGES += libprotobuf-cpp-full
 
