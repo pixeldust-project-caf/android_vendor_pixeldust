f15f94ce72d6e7a3acfafbb10acbc537d57521d5
Paul Keith <javelinanddart@gmail.com>
2018-02-15 21:46:00 +0100

    vendor: build: Add Target_shim_libs
    Change-Id: I4a191f8ce61123e30f780f8e39411a7723fb6197
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,7 +1,7 @@
 _contents := $(_contents)    "Pixeldust":{$(newline)
 
 # See build/core/soong_config.mk for the add_json_* functions you can use here.
-
+$(call add_json_str,  Target_shim_libs,                      $(TARGET_LD_SHIM_LIBS))
 
 # This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
 _contents := $(_contents)__SV_END
