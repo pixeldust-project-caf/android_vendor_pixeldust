352fbba9d7faf4ce71c58c944a37a23e12d77808
Rashed Abdel-Tawab <rashed@linux.com>
2018-03-24 11:17:49 -0700

    soong: Add TARGET_USES_QTI_CAMERA_DEVICE
    Change-Id: I925b137edeed9fa11bbe566002147b444328c661
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -50,20 +50,25 @@ type Product_variables struct {
 		Header_libs []string
 		Shared_libs []string
 	}
+	Uses_qti_camera_device struct {
+		Cppflags []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
-	Additional_gralloc_10_usage_bits 			*string `json:",omitempty"`
-	Should_skip_waiting_for_qsee 				*bool `json:",omitempty"`
-	Supports_hw_fde 					*bool `json:",omitempty"`
-	Supports_hw_fde_perf 					*bool `json:",omitempty"`
- 	Target_init_vendor_lib 					*string `json:",omitempty"`
-	Target_process_sdk_version_override 			*string `json:",omitempty"`
-	Target_shim_libs 					*string `json:",omitempty"`
-	Target_uses_color_metadata				*bool `json:",omitempty"`
-	Uses_qcom_um_family					*bool `json:",omitempty"`
-	Uses_qcom_um_3_18_family				*bool `json:",omitempty"`
-	Uses_qcom_um_4_4_family					*bool `json:",omitempty"`
-	Uses_qcom_um_4_9_family					*bool `json:",omitempty"`
-	Uses_qcom_um_4_14_family				*bool `json:",omitempty"`
+	Additional_gralloc_10_usage_bits        *string `json:",omitempty"`
+	Should_skip_waiting_for_qsee            *bool `json:",omitempty"`
+	Supports_hw_fde                         *bool `json:",omitempty"`
+	Supports_hw_fde_perf                    *bool `json:",omitempty"`
+ 	Target_init_vendor_lib                  *string `json:",omitempty"`
+	Target_process_sdk_version_override     *string `json:",omitempty"`
+	Target_shim_libs                        *string `json:",omitempty"`
+	Target_uses_color_metadata              *bool `json:",omitempty"`
+	Uses_qcom_um_family                     *bool `json:",omitempty"`
+	Uses_qcom_um_3_18_family                *bool `json:",omitempty"`
+	Uses_qcom_um_4_4_family                 *bool `json:",omitempty"`
+	Uses_qcom_um_4_9_family                 *bool `json:",omitempty"`
+	Uses_qcom_um_4_14_family                *bool `json:",omitempty"`
+	Uses_qti_camera_device                  *bool `json:",omitempty"`
 }
