06ca97858e735bdfe6fc81871c23d27e3ac6fdf0
spezi77 <spezi7713@gmx.net>
2018-08-12 18:19:40 +0200

    Create overlay for fw_base.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_main.mk
+++ configs/pixeldust_main.mk
@@ -12,6 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Include overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/pixeldust/overlay
+
 # Include PixelDust device specific sepolicies
 -include device/pixeldust/sepolicy/common/sepolicy.mk
 
