a78aeb86e3963cd608f4e42eeebce228fd240c78
Kevin F. Haggerty <haggertk@lineageos.org>
2018-08-25 20:23:19 -0600

    soong_config: Allow extension of valid gralloc 1.0 buffer usage bits
    * fw/native change I7f4174581e24e361577640b9263514a168ed482d
      implemented validation of the buffer description info prior to
      creating the descriptor. Some of our legacy devices need to
      whitelist additional usage bits to support various functionality.
    * The TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS variable can contain
      a singular roll-up value (e.g., 0x02000000U), a left-shifted bit
      (e.g., (1 << 25)), a bitwise OR of these things, or essentially
      anything that the compiler can choke down.
    
    Change-Id: I2127d33b03426b5e0f981aae837e07d82163fa17
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,5 +1,8 @@
 package android
 type Product_variables struct {
+	Additional_gralloc_10_usage_bits struct {
+		Cppflags []string
+	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
 	}
@@ -36,10 +39,11 @@ type Product_variables struct {
 }
 
 type ProductVariables struct {
+	Additional_gralloc_10_usage_bits 		*string `json:",omitempty"`
 	Target_init_vendor_lib					*string `json:",omitempty"`
-	Target_shim_libs					*string `json:",omitempty"`
+	Target_shim_libs						*string `json:",omitempty"`
 	Target_uses_color_metadata				*bool `json:",omitempty"`
-	Uses_qcom_um_family					*bool `json:",omitempty"`
+	Uses_qcom_um_family						*bool `json:",omitempty"`
 	Uses_qcom_um_3_18_family				*bool `json:",omitempty"`
 	Uses_qcom_um_4_4_family					*bool `json:",omitempty"`
 	Uses_qcom_um_4_9_family					*bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,8 +1,9 @@
 $(call add_json_map, Pixeldust)
 
-$(call add_json_str_omitempty,	Target_init_vendor_lib,					$(TARGET_INIT_VENDOR_LIB))
-$(call add_json_str,		Target_shim_libs, 					$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
-$(call add_json_bool,		Target_uses_color_metadata, 				$(filter true,$(TARGET_USES_COLOR_METADATA)))
+$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_str_omitempty,	Target_init_vendor_lib,				$(TARGET_INIT_VENDOR_LIB))
+$(call add_json_str,		Target_shim_libs, 						$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
+$(call add_json_bool,		Target_uses_color_metadata, 			$(filter true,$(TARGET_USES_COLOR_METADATA)))
 $(call add_json_bool,		Uses_qcom_um_family, 					$(filter true,$(TARGET_USES_QCOM_UM_FAMILY)))
 $(call add_json_bool,		Uses_qcom_um_3_18_family, 				$(filter true,$(TARGET_USES_QCOM_UM_3_18_FAMILY)))
 $(call add_json_bool,		Uses_qcom_um_4_4_family, 				$(filter true,$(TARGET_USES_QCOM_UM_4_4_FAMILY)))
