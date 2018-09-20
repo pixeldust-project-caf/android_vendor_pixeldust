c62218840a9800dd55d7b5e5f248934c27756885
Rashed Abdel-Tawab <rashed@linux.com>
2018-09-20 15:19:57 -0700

    build: Move some kernel definitions to BoardConfigKernel
    Kernel source settings should always come at BoardConfig population
    time so things that use the variable later don't end up pointing to
    an empty or wrong variable.
    
    The following is also squashed in:
    
    Author: Christopher N. Hesse <raymanfx@gmail.com>
    Date:   Fri Aug 10 00:23:54 2018 +0200
        tasks: kernel: Honor prebuilt kernel flag
        For devices that want to use a prebuilt kernel, TARGET_KERNEL_SOURCE
        would still be set to TARGET_AUTO_KDIR, meaning the build system would
        still try to build the kernel if TARGET_AUTO_KDIR was present.
        Setting TARGET_PREBUILT_KERNEL indicates this is not wanted, so don't
        attempt to do it.
        Change-Id: Ic79b3ac1b9c946fd258ada43dce2b08bb74ea0d9
    
    Change-Id: If046b86ff0d18c76898e90295be873a8379f678a
    
    [spezi77] Moved BoardConfigs to build/board
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/board/BoardConfigPD.mk
+++ build/board/BoardConfigPD.mk
@@ -3,5 +3,10 @@
 #    SPDX-License-Identifier: Apache-2.0
 #
 
+# Kernel
+ifneq ($(TARGET_NO_KERNEL),true)
+include vendor/pixeldust/build/board/BoardConfigKernel.mk
+endif
+
 # Soong
 include vendor/pixeldust/build/board/BoardConfigSoong.mk
