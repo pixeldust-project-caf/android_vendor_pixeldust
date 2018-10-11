9e8d323c9ae1e9d08b622ce7848a3f4a151730a8
Alessandro Astone <ales.astone@gmail.com>
2018-10-12 00:16:27 +0200

    soong_config: Add BOOTLOADER_MESSAGE_OFFSET
    * libbootloader_message now uses Android.bp
    
    Change-Id: I750a04be97dbe01564d45cd605744b15f209e408
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -3,6 +3,9 @@ type Product_variables struct {
 	Additional_gralloc_10_usage_bits struct {
 		Cppflags []string
 	}
+	Bootloader_message_offset struct {
+		Cflags []string
+	}
 	Target_init_vendor_lib struct {
 		Whole_static_libs []string
 	}
@@ -40,6 +43,7 @@ type Product_variables struct {
 
 type ProductVariables struct {
 	Additional_gralloc_10_usage_bits 		*string `json:",omitempty"`
+	Bootloader_message_offset               *int `json:",omitempty"`
 	Target_init_vendor_lib					*string `json:",omitempty"`
 	Target_shim_libs						*string `json:",omitempty"`
 	Target_uses_color_metadata				*bool `json:",omitempty"`
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -1,6 +1,9 @@
+add_json_val_default = $(call add_json_val, $(1), $(if $(strip $(2)), $(2), $(3)))
+
 $(call add_json_map, Pixeldust)
 
-$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,	$(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_str_omitempty,	Additional_gralloc_10_usage_bits,   $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS))
+$(call add_json_val_default,    Bootloader_message_offset,          $(BOOTLOADER_MESSAGE_OFFSET), 0)
 $(call add_json_str_omitempty,	Target_init_vendor_lib,				$(TARGET_INIT_VENDOR_LIB))
 $(call add_json_str,		Target_shim_libs, 						$(subst $(space),:,$(TARGET_LD_SHIM_LIBS)))
 $(call add_json_bool,		Target_uses_color_metadata, 			$(filter true,$(TARGET_USES_COLOR_METADATA)))
