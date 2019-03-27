df6262caec998a4818c703abc8240de19a3c039c
spezi77 <spezi7713@gmx.net>
2019-03-27 22:20:37 +0100

    Recommend using the non debug dexpreopter.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_optimizations.mk
+++ configs/pixeldust_optimizations.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# Recommend using the non debug dexpreopter
+USE_DEX2OAT_DEBUG := false
+
 ifneq ($(HOST_OS),linux)
 ifneq ($(sdclang_already_warned),true)
 $(warning **********************************************)
