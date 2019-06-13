3ceafd491bce10f8d023803e7ff02ab040b7bad8
Sonal Singh <sonal.singh.19993@gmail.com>
2019-06-13 12:56:33 +0000

    vendor: products: Add X01BD
    Signed-off-by: spezi77 <spezi7713@gmx.net>

---
+++ products/X01BD/pixeldust_X01BD.mk
@@ -0,0 +1,64 @@
+# Copyright (C) 2019 The PixelDust Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2280
+
+# Release name
+PRODUCT_RELEASE_NAME := Zenfone Max Pro M2
+export TARGET_DEVICE := X01BD
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+# Inherit from X01BD device
+$(call inherit-product, device/asus/X01BD/device.mk)
+
+# Generic CAF packages
+include device/qcom/common/common.mk
+
+# Include common PixelDust stuff
+include vendor/pixeldust/configs/pixeldust_phone.mk
+
+# Include optional stuff (e.g. prebuilt apps)
+include vendor/pixeldust/configs/system_optional.mk
+
+# Google Apps
+$(call inherit-product, vendor/googleapps/googleapps.mk)
+
+# Setup device specific product configuration.
+PRODUCT_NAME := pixeldust_X01BD
+PRODUCT_DEVICE := X01BD
+PRODUCT_BRAND := asus
+PRODUCT_MODEL := Zenfone Max Pro M2
+PRODUCT_MANUFACTURER := asus
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
+
+TARGET_VENDOR := asus
+TARGET_VENDOR_PRODUCT_NAME := X01BD
+TARGET_VENDOR_DEVICE_NAME := X01BD
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ3A.190505.002 5450365 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "google/crosshatch/crosshatch:9/PQ3A.190505.002/5450365:user/release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
