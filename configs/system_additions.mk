809e2fad8d61e996f34040a7593d1b19e7fe07f5
spezi77 <spezi7713@gmx.net>
2019-06-18 13:23:32 +0530

    Build Live Wallpaper Picker for all devices. Signed-off-by: amarbajpai <bajpai.amar3@gmail.com>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -61,11 +61,14 @@ include vendor/themes/common.mk
 # Extra Packages
 PRODUCT_PACKAGES += \
     Launcher3QuickStep \
-    NoCutoutOverlay
+    NoCutoutOverlay \
+    LiveWallpapers \
+    LiveWallpapersPicker
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/pixeldust/prebuilt/bin/backuptool.sh:install/bin/backuptool.sh \
     vendor/pixeldust/prebuilt/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/pixeldust/prebuilt/bin/50-base.sh:system/addon.d/50-base.sh \
+
 
