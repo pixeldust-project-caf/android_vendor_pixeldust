c554d204b9000702fc2544dea9e4f9b252384f9b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-07-16 15:58:42 -0300

    vendor: Add qcom platform type to soongs vars
    Required for new commits on frameworks/native
    
    * Fix lag during screen recording on some platforms
    * Fix screen flickering after disconnecting from Android Auto on some platforms
    
    Change-Id: Ia0ed0bbfda44fb5ccb184ace854ec1bf9a7d819a
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -6,9 +6,38 @@ type Product_variables struct {
 	Target_uses_color_metadata struct {
 		Cppflags []string
 	}
+	Uses_qcom_um_family struct {
+		Cflags []string
+		Srcs []string
+	}
+	Uses_qcom_um_3_18_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_4_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_9_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
+	Uses_qcom_um_4_14_family struct {
+		Include_dirs []string
+		Header_libs []string
+		Shared_libs []string
+	}
 }
 
 type ProductVariables struct {
-	Target_shim_libs  *string `json:",omitempty"`
-	Target_uses_color_metadata  *bool `json:",omitempty"`
+	Target_shim_libs					*string `json:",omitempty"`
+	Target_uses_color_metadata				*bool `json:",omitempty"`
+	Uses_qcom_um_family					*bool `json:",omitempty"`
+	Uses_qcom_um_3_18_family				*bool `json:",omitempty"`
+	Uses_qcom_um_4_4_family					*bool `json:",omitempty"`
+	Uses_qcom_um_4_9_family					*bool `json:",omitempty"`
+	Uses_qcom_um_4_14_family				*bool `json:",omitempty"`
 }
