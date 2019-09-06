1de1311416a8203a3be14db727eb98ecc51c330e
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-06 10:44:50 -0700

    kernel: Use prebuilt binaries
    perl, lz4c, and a bunch of other binaries are not allowed anymore.
    So we need to provide our own.
    
    spezi77 edit: Use our prebuilts/tools-customs instead of tools-lineage.
    
    Change-Id: Idbc88b76247b3dda7c5d52e5eed2d548c33f6f2d
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- config/BoardConfigKernel.mk
+++ config/BoardConfigKernel.mk
@@ -128,3 +128,8 @@ else
   KERNEL_MAKE_FLAGS += HOSTCFLAGS="-L $(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib/x86_64-linux-gnu"
   KERNEL_MAKE_FLAGS += HOSTLDFLAGS="-L $(BUILD_TOP)/prebuilts/openssl/$(HOST_OS)-x86/1.1.1/lib/x86_64-linux-gnu"
 endif
+
+TOOLS_PATH_OVERRIDE := \
+    PATH=$(BUILD_TOP)/prebuilts/tools-custom/$(HOST_OS)-x86/bin:$$PATH \
+    LD_LIBRARY_PATH=$(BUILD_TOP)/prebuilts/tools-custom/$(HOST_OS)-x86/lib:$$LD_LIBRARY_PATH \
+    PERL5LIB=$(BUILD_TOP)/prebuilts/tools-custom/$(HOST_OS)-x86/lib/perl-base
