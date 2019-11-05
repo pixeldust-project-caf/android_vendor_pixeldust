7d107532bba6615ba4ea5e9ed6a3776b01b7c85c
nitin1438 <nitin.chobhe@gmail.com>
2019-11-05 16:15:14 +0530

    Revert "Add AmbientSensePrebuilt: it's used also by Google app/SoundSearch tile"
    This reverts commit a0665848a24c8f84a591737e8cedb22a3f477136.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- prebuilt/priv-app/Android.mk
+++ prebuilt/priv-app/Android.mk
@@ -15,17 +15,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := AmbientSensePrebuilt
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := AmbientSensePrebuilt/AmbientSensePrebuilt.apk
-LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := PixelThemes
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := PixelThemes/PixelThemes.apk
