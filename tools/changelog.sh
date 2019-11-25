5aa11018f6cd4053f9a32273057ede5f15228401
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-25 03:55:38 +0200

    changelog: Only fetch necessary dates
     * to save time upon dirty building
     * include the last 7 days instead of 30 days
    
    note: re-generating days is possible.
    Just delete the dates you want regenerated from the top (!!) of the
    changelog file in $OUT/system/etc/Changelog.txt
    2nd line of the file must be the date to fetch from!!
    
    spezi77 edits: Include the last 30 days instead of 7 days coz we do
    monthly releases usually.
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,17 +1,48 @@
-#!/bin/sh
+#!/bin/bash
+# note: re-generating days is possible. Just delete the dates you want regenerated
+# from the top (!!) of the changelog file in $OUT/system/etc/Changelog.txt
+# 2nd line of the file must be the date to fetch from!!
 
 export Changelog=Changelog.txt
+export PassedDays=30 # change this to limit max changelog days
 
-if [ -f $Changelog ];
+if [ -f "${OUT}/system/etc/${Changelog}" ] # if changelog already generated
 then
-	rm -f $Changelog
+	cp "${OUT}/system/etc/${Changelog}" $Changelog
+	LastDate=`sed '2!d' $Changelog` # get 2nd line of changelog file
+	LastDate="$(echo -e "${LastDate}" | tr -d '[:space:]')" # get rid of whitespaces
+	# extract month day and year from string:
+	Month=${LastDate:0:2}
+	Day=${LastDate:3:2}
+	Year=${LastDate:6:4}
+	LastDate=`date -d "${Year}${Month}${Day}" +%s`
+	TimeNow=`date +%s` # current time
+	DayDiff=$(( (TimeNow - LastDate) / 86400 )) # n/o days passed
+	if [[ $DayDiff < $PassedDays ]]; then # don't fetch more than max days
+		export PassedDays=$DayDiff
+		mv $Changelog "${Changelog}.bak" # save current changelog for later
+	fi
+	touch $Changelog
+else
+	touch $Changelog
 fi
 
-touch $Changelog
+if [[ $PassedDays == 0 ]]; then
+	echo "Already have today"
+	rm $Changelog
+	rm "${Changelog}.bak"
+	exit 0
+else
+	echo "Regenerating log of last ${PassedDays} days"
+	if [ -f "${OUT}/system/etc/${Changelog}" ]; then
+		rm "${OUT}/system/etc/${Changelog}"
+		rm "${OUT}/${Changelog}"
+	fi
+fi
 
 echo "Generating changelog..."
 
-for i in $(seq 30);
+for i in $(seq $PassedDays);
 do
 export After_Date=`date --date="$i days ago" +%m-%d-%Y`
 k=$(expr $i - 1)
@@ -19,7 +50,7 @@ k=$(expr $i - 1)
 
 	# Line with after --- until was too long for a small ListView
 	echo '====================' >> $Changelog;
-	echo  "     "$Until_Date    >> $Changelog;
+	echo "     "$Until_Date     >> $Changelog;
 	echo '====================' >> $Changelog;
 	echo >> $Changelog;
 
@@ -29,6 +60,12 @@ k=$(expr $i - 1)
 done
 
 sed -i 's/project/   */g' $Changelog
+
+if [ -f "${Changelog}.bak" ]
+then
+	cat "${Changelog}.bak" >> $Changelog # restore old changelog to the end of current one
+	rm "${Changelog}.bak"
+fi
 
 cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/
