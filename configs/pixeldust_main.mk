ed7cc396deb89b00bfee6ae484f4176490c37c38
spezi77 <spezi7713@gmx.net>
2019-12-24 17:28:42 +0100

    Update privapp permissions whitelist. Also copy the whitelist to the product partition for the devices that require it (e.g. 3rd and 4th gen pixels).
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_main.mk
+++ configs/pixeldust_main.mk
@@ -22,6 +22,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 
 # Include needed privapp permissions whitelist
 PRODUCT_COPY_FILES += vendor/pixeldust/prebuilt/etc/privapp-permissions/pixeldust-permissions.xml:system/etc/permissions/pixeldust-permissions.xml
+PRODUCT_COPY_FILES += vendor/pixeldust/prebuilt/etc/privapp-permissions/pixeldust-permissions.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/pixeldust-permissions.xml
 
 # Copy all init rc files
 $(foreach f,$(wildcard vendor/pixeldust/prebuilt/etc/init/*.rc),\
