7f8d75eba5d3d46810fc1a16ed71b5cd63ff03ef
spezi77 <spezi7713@gmx.net>
2020-02-26 00:38:50 +0100

    privapp-permissions: Add screenrecorder to whitelist.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/system_additions.mk
+++ configs/system_additions.mk
@@ -71,6 +71,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/pixeldust/config/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
 
+# Screenrecorder permissions
+PRODUCT_COPY_FILES += \
+    vendor/pixeldust/config/permissions/privapp-permissions-screenrecorder.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-screenrecorder.xml
+
 # Hidden API whitelist
 PRODUCT_COPY_FILES += \
     vendor/pixeldust/config/permissions/pdx-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/pdx-hiddenapi-package-whitelist.xml
