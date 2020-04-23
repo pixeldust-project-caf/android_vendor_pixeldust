7824fbf625a85bb4b9d252b3d0f1ee242d5ac4bb
PixelBoot <bootingpixels@gmail.com>
2020-04-23 11:04:14 +0200

    angler: Revert to older build fingerprint
    Platform spl date doesn't match stock rom, which causes cts
    profile mismatch. Using a March 2018 or older build fingerprint
    results in cts profile check not caring about spl date.

--- products/angler/pixeldust_angler.mk
+++ products/angler/pixeldust_angler.mk
@@ -44,11 +44,15 @@ PRODUCT_DEVICE := angler
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := Huawei
 
+# Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=angler \
-    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM7.181205.001 5080180 release-keys"
+    PRIVATE_BUILD_DESC="angler-user 8.1.0 OPM3.171019.014 4503998 release-keys"
 
-BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM7.181205.001/5080180:user/release-keys
+BUILD_FINGERPRINT := google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/angler/angler:8.1.0/OPM3.171019.014/4503998:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="PixelBoot" \
