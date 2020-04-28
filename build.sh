75dcb57113c27e45b9d9332e3f8dd028afd580a3
spezi77 <spezi7713@gmx.net>
2020-04-28 15:19:40 +0200

    build: Remove the build time redundant to AOSP.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build.sh
+++ build.sh
@@ -241,11 +241,4 @@ if [ $RETVAL -ne 0 ]; then
         echo -e ""
 fi
 
-# Check the finishing time
-TIME_END=$(date +%s.%N)
-
-# Log those times at the end as a fun fact of the day
-echo -e "${CLR_BLD_GRN}Total time elapsed:${CLR_RST} ${CLR_GRN}$(echo "($TIME_END - $TIME_START) / 60" | bc) minutes ($(echo "$TIME_END - $TIME_START" | bc) seconds)${CLR_RST}"
-echo -e ""
-
 exit $RETVAL
