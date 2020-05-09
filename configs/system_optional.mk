b62dede8eb14e9aa2997ca01275c901df6d2c8ad
spezi77 <spezi7713@gmx.net>
2020-05-09 13:06:16 +0200

    Build AwakeWallpaper created by @victorlapin and @ROGERdotT Todo: Set as default wallpaper
    Example:
        <!-- Component name of the default wallpaper. This will be ImageWallpaper if not specified -->
        <string name="default_wallpaper_component" translatable="false">com.breel.wallpapers19/com.breel.wallpapers19.doodle.wallpaper.variations.DoodleWallpaperV1</string>
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/system_optional.mk
+++ configs/system_optional.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2018 The Pixel Dust Project
+# Copyright (C) 2018-2020 The PixelDust Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Build PixelDust Walls
+# Build PixelDust Goodies
 PRODUCT_PACKAGES += \
+    AwakeWallpaper \
     PixelDustWallpapers
 
