dbec989a2aad733163de040a6c30d001b1517a21
nitin1438 <nitin.chobhe@gmail.com>
2020-05-17 22:24:53 +0530

    taimen: make file clean-up

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 The PixelDust Project
+# Copyright (C) 2020 The PixelDust Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,56 +12,36 @@
 # See the License for the specific language governing permissions and
 #  limitations under the License.
 
-# Bootanimation
+# Boot animation
 BOOTANIMATION := 1440
 
 # Release name
 PRODUCT_RELEASE_NAME := Pixel2XL
 export TARGET_DEVICE := taimen
 
-# We have our own power HAL
-TARGET_USES_DEVICE_SPECIFIC_POWERHAL := true
-
-# Use the AOSP stack
-TARGET_USES_AOSP := true
-
-# Use the sepolicies which are being shipped with our device
-TARGET_EXCLUDE_QCOM_VENDOR_SEPOLICY := true
-
-# Inherit device configuration
-$(call inherit-product-if-exists, device/google/taimen/aosp_taimen.mk)
-
-# Generic CAF packages
-#include device/qcom/common/common.mk
-
-# Include common PixelDust stuff
-include vendor/pixeldust/configs/pixeldust_phone.mk
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/pixeldust/configs/pixeldust_phone.mk)
 
 # Include optional stuff (e.g. prebuilt apps)
 include vendor/pixeldust/configs/system_optional.mk
 
-# Include pixel ambient sense (Now playing)
-#include vendor/pixeldust/configs/ambientsense.mk
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
-# Google Apps
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
-REMOVE_GAPPS_PACKAGES += \
-    PrebuiltGmail \
-    NexusLauncherRelease
+-include device/google/wahoo/device-pixeldust.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := pixeldust_taimen
 PRODUCT_DEVICE := taimen
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
-TARGET_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="taimen" \
-    TARGET_DEVICE="taimen" \
-    PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200501.001.B2 6352890 release-keys"
+	PRODUCT_NAME="taimen" \
+	PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200501.001.B3 6396602 release-keys"
 
-BUILD_FINGERPRINT := "google/taimen/taimen:10/QQ2A.200501.001.B2/6352890:user/release-keys"
+BUILD_FINGERPRINT := google/taimen/taimen:10/QQ2A.200501.001.B3/6396602:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="nitin1438" \
@@ -72,3 +52,9 @@ PRODUCT_COPY_FILES += \
     vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
+REMOVE_GAPPS_PACKAGES += \
+	PrebuiltGmail \
+	MatchmakerPrebuiltPixel4 \
+	WellbeingPrebuilt
