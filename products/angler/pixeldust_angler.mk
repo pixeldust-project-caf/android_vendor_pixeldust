05afbcef3b8a9593b3346b6ce98ec7caf03c9808
PixelBoot <bootingpixels@gmail.com>
2020-06-19 23:24:03 +0200

    angler: exclude unnecessary sepolicy

--- products/angler/pixeldust_angler.mk
+++ products/angler/pixeldust_angler.mk
@@ -35,27 +35,6 @@ REMOVE_GAPPS_PACKAGES += \
     PrebuiltGmail \
     NexusLauncherRelease
 
-# MSM8994 Display HAL settings
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.composition.type=c2d \
-    debug.sf.hw=1 \
-    persist.debug.wfd.enable=1 \
-    persist.demo.hdmirotationlock=false \
-    persist.hwc.enable_vds=1 \
-    persist.hwc.mdpcomp.enable=true \
-    persist.hwc.ptor.enable=true \
-    persist.mdpcomp.4k2kSplit=1 \
-    persist.hwc.mdpcomp.maxpermixer=5 \
-    persist.mdpcomp_perfhint=50 \
-    persist.metadata_dynfps.disable=true \
-    persist.sys.wfd.virtual=0
-
-PRODUCT_PACKAGES += \
-    copybit.msm8994
-
-# Exclude QCOM SEPolicy
-TARGET_EXCLUDE_QCOM_SEPOLICY := true
-
 # Setup device specific product configuration.
 PRODUCT_NAME := pixeldust_angler
 PRODUCT_BRAND := google
