595901460f04c196cc00fb3c83a6b90b859569f7
nitin1438 <nitin.chobhe@gmail.com>
2020-07-07 14:33:53 +0530

    taimen/ walleye: Update build fingerprint to QQ3A.200705.002

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -39,9 +39,9 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_NAME="taimen" \
-	PRIVATE_BUILD_DESC="taimen-user 10 QQ3A.200605.001 6392402 release-keys"
+	PRIVATE_BUILD_DESC="taimen-user 10 QQ3A.200705.002 6506677 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:10/QQ3A.200605.001/6392402:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:10/QQ3A.200705.002/6506677:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="nitin1438" \
--- products/walleye/pixeldust_walleye.mk
+++ products/walleye/pixeldust_walleye.mk
@@ -18,9 +18,9 @@ TARGET_DEVICE := walleye
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    PRIVATE_BUILD_DESC="walleye-user 10 QQ3A.200605.001 6392402 release-keys"
+    PRIVATE_BUILD_DESC="walleye-user 10 QQ3A.200705.002 6506677 release-keys"
 
-BUILD_FINGERPRINT := google/walleye/walleye:10/QQ3A.200605.001/6392402:user/release-keys
+BUILD_FINGERPRINT := google/walleye/walleye:10/QQ3A.200705.002/6506677:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/walleye/walleye-vendor.mk)
 $(call inherit-product-if-exists, vendor/gapps/gapps.mk)
