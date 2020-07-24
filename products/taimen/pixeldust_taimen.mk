4f63ce6e93712ba7e9319f695538816879ac19a4
nitin1438 <nitin.chobhe@gmail.com>
2020-07-24 21:41:17 +0530

    taimen: bring back taimen to PD-CAF ;)
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2019 The PixelDust Project
+# Copyright (C) 2019-2020 The PixelDust Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -10,7 +10,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
-#  limitations under the License.
+# limitations under the License.
 
 # Bootanimation
 BOOTANIMATION := 1440
@@ -19,20 +19,19 @@ BOOTANIMATION := 1440
 PRODUCT_RELEASE_NAME := Pixel2XL
 export TARGET_DEVICE := taimen
 
-# We have our own power HAL
-TARGET_USES_DEVICE_SPECIFIC_POWERHAL := true
-
-# Use the AOSP stack
-TARGET_USES_AOSP := true
-
 # Use the sepolicies which are being shipped with our device
 TARGET_EXCLUDE_QCOM_VENDOR_SEPOLICY := true
 
-# Inherit device configuration
-$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+# Indicate that we use AOSP surfaceflinger
+TARGET_USE_AOSP_SURFACEFLINGER := true
 
-# Generic CAF packages
-#include device/qcom/common/common.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+$(call inherit-product-if-exists, device/google/wahoo/device-pixeldust.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Include common PixelDust stuff
 include vendor/pixeldust/configs/pixeldust_phone.mk
@@ -40,28 +39,18 @@ include vendor/pixeldust/configs/pixeldust_phone.mk
 # Include optional stuff (e.g. prebuilt apps)
 include vendor/pixeldust/configs/system_optional.mk
 
-# Include pixel ambient sense (Now playing)
-#include vendor/pixeldust/configs/ambientsense.mk
-
-# Google Apps
-$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
-REMOVE_GAPPS_PACKAGES += \
-    PrebuiltGmail \
-    NexusLauncherRelease
-
 # Device identifier. This must come after all inclusions
 PRODUCT_NAME := pixeldust_taimen
 PRODUCT_DEVICE := taimen
-PRODUCT_BRAND := Google
+PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
-TARGET_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="taimen" \
-    TARGET_DEVICE="taimen" \
-    PRIVATE_BUILD_DESC="taimen-user 10 QQ2A.200501.001.B2 6352890 release-keys"
+	PRODUCT_NAME="taimen" \
+	PRIVATE_BUILD_DESC="taimen-user 10 QQ3A.200705.002 6506677 release-keys"
 
-BUILD_FINGERPRINT := "google/taimen/taimen:10/QQ2A.200501.001.B2/6352890:user/release-keys"
+BUILD_FINGERPRINT := google/taimen/taimen:10/QQ3A.200705.002/6506677:user/release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.pixeldust.maintainer="nitin1438" \
@@ -72,3 +61,10 @@ PRODUCT_COPY_FILES += \
     vendor/pixeldust/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+$(call inherit-product-if-exists, vendor/gapps/gapps.mk)
+
+REMOVE_GAPPS_PACKAGES += \
+	PrebuiltGmail \
+	MatchmakerPrebuiltPixel4 \
+	NexusLauncherRelease
+
