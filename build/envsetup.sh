e1c56a7a96b75442d8df1227e3b82072bab24b65
spezi77 <spezi7713@gmx.net>
2020-09-10 00:26:57 +0200

    build: tasks: Change bacon -> pixeldust and adapt it to our base.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -52,7 +52,7 @@ function brunch()
 {
     breakfast $*
     if [ $? -eq 0 ]; then
-        mka bacon
+        mka pixeldust
     else
         echo "No such item in brunch menu. Try 'breakfast'"
         return 1
@@ -421,7 +421,7 @@ function cmka() {
     if [ ! -z "$1" ]; then
         for i in "$@"; do
             case $i in
-                bacon|otapackage|systemimage)
+                pixeldust|otapackage|systemimage)
                     mka installclean
                     mka $i
                     ;;
