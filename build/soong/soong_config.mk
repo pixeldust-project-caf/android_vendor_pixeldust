PotatoProject/vendor_potato

Revert "soong_config: Switch to json_map calls"
This reverts commit 0cf4ed1b933913fad8185dba8813c6764526397c.

Change-Id: I65b5b69314132ff86c20dce7da12d1ee741bc5cd

build/soong/soong_config.mk
@@ -1,4 +1,4 @@
-$(call add_json_map, Potato)
+_contents := $(_contents)    "Potato":{$(newline)
 
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
@@ -14,4 +14,7 @@ $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
-$(call end_json_map)
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)

pixeldust-project-caf/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi7713@gmx.net>

build/soong/soong_config.mk
@@ -1,4 +1,4 @@
-$(call add_json_map, Pixeldust)
+_contents := $(_contents)    "Pixeldust":{$(newline)
 
 $(call add_json_bool, Has_legacy_camera_hal1, $(filter true,$(TARGET_HAS_LEGACY_CAMERA_HAL1)))
 $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
@@ -14,4 +14,7 @@ $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
-$(call end_json_map)
+# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
+_contents := $(_contents)__SV_END
+
+_contents := $(_contents)    },$(newline)