PotatoProject/vendor_potato

soong_config: Switch to json_map calls
Change-Id: I8b18665e40bb12c532c119e7a59b5848a1f9fe75
Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

build/soong/soong_config.mk
@@ -1,6 +1,5 @@
-_contents := $(_contents)    "Potato":{$(newline)
+$(call add_json_map, Potato)
 
-# See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
@@ -13,7 +12,4 @@ $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
-# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
-
-_contents := $(_contents)    },$(newline)
+$(call end_json_map)

pixeldust-project-caf/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi7713@gmx.net>

build/soong/soong_config.mk
@@ -1,6 +1,5 @@
-_contents := $(_contents)    "Pixeldust":{$(newline)
+$(call add_json_map, Pixeldust)
 
-# See build/core/soong_config.mk for the add_json_* functions you can use here.
 $(call add_json_bool, Device_support_hwfde, $(filter true,$(TARGET_HW_DISK_ENCRYPTION)))
 $(call add_json_bool, Device_support_hwfde_perf, $(filter true,$(TARGET_HW_DISK_ENCRYPTION_PERF)))
 $(call add_json_bool, Device_support_legacy_hwfde, $(filter true,$(TARGET_LEGACY_HW_DISK_ENCRYPTION)))
@@ -13,7 +12,4 @@ $(call add_json_str, Target_shim_libs, $(TARGET_LD_SHIM_LIBS))
 $(call add_json_bool, Uses_generic_camera_parameter_library, $(if $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY),false,true))
 $(call add_json_bool, Uses_qcom_bsp_legacy, $(filter true,$(TARGET_USES_QCOM_BSP_LEGACY)))
 
-# This causes the build system to strip out the last comma in our nested struct, to keep the JSON valid.
-_contents := $(_contents)__SV_END
-
-_contents := $(_contents)    },$(newline)
+$(call end_json_map)