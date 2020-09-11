1ba31a1c951e6e476178299c44d24f6314d53af1
Dyneteve <dyneteve@hentaios.com>
2020-09-11 19:48:02 +0200

    vendor/apex: Get things rolling
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/apex.mk
+++ configs/apex.mk
@@ -18,6 +18,26 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/pixeldust/apex
 
+# ModuleMetadata
 PRODUCT_PACKAGES += \
     ModuleMetadataGoogle
 
+# Google Apexes
+PRODUCT_PACKAGES += \
+    com.google.android.adbd \
+	com.google.android.cellbroadcast \
+	com.google.android.conscrypt \
+	com.google.android.extservices \
+	com.google.android.ipsec \
+	com.google.android.media \
+	com.google.android.media.swcodec \
+	com.google.android.mediaprovider \
+	com.google.android.neuralnetworks \
+	com.google.android.os.statsd \
+	com.google.android.permission \
+	com.google.android.resolv \
+	com.google.android.sdkext \
+	com.google.android.telephony \
+	com.google.android.tethering \
+	com.google.android.tzdata2 \
+	com.google.android.wifi
