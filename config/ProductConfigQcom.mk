97894863c5ebebf274fc654ca1ecabf6cc5ca575
Pig <pig.priv@gmail.com>
2020-09-19 08:44:33 +0800

    config: Setup soong namespaces for TARGET_USE_QTI_BT_STACK
     * Ref: [https://github.com/LineageOS/android_vendor_qcom_opensource_bluetooth-commonsys-intf/blob/lineage-18.0/bt-system-opensource-product.mk]
    
    Change-Id: I6bf3e1dda6fe5dc66f6fafdb32a1daecb9616c84
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- config/ProductConfigQcom.mk
+++ config/ProductConfigQcom.mk
@@ -104,4 +104,10 @@ ifneq ($(USE_DEVICE_SPECIFIC_DATA_IPA_CFG_MGR),true)
     PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/data-ipa-cfg-mgr
 endif
 
+ifeq ($(TARGET_USE_QTI_BT_STACK),true)
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
+endif #TARGET_USE_QTI_BT_STACK
+
 include vendor/pixeldust/build/core/qcom_target.mk
