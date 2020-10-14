29e7a3609e122a1f2ea518a20a9eee2819c258ec
Pig <pig.priv@gmail.com>
2020-10-15 02:58:37 +0800

    soong: add Pixeldust soong config variables
    Ref: https://android.googlesource.com/platform/build/soong/+/refs/tags/android-11.0.0_r4#soong-config-variables
    
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/board/BoardConfigSoong.mk
+++ build/board/BoardConfigSoong.mk
@@ -15,8 +15,15 @@ endef
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
 
 SOONG_CONFIG_NAMESPACES += pixeldustGlobalVars
-SOONG_CONFIG_pixeldustGlobalVars +=
+SOONG_CONFIG_pixeldustGlobalVars += \
+    target_init_vendor_lib \
+    target_ld_shim_libs \
+    target_process_sdk_version_override
 
 # Set default values
+TARGET_INIT_VENDOR_LIB ?= vendor_init
 
 # Soong value variables
+SOONG_CONFIG_pixeldustGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
+SOONG_CONFIG_pixeldustGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
+SOONG_CONFIG_pixeldustGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
