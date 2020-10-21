c69df5aac791796615664d41d1938d2f89d58614
Volodymyr Zhdanov <wight554@gmail.com>
2020-10-22 01:33:24 +0300

    extract_utils: implement patchelf
    also includes:
    
    From 490e38efd748f0c623baab1e4cb1f4adb34f43d1 Mon Sep 17 00:00:00 2001
    From: Sebastiano Barezzi <barezzisebastiano@gmail.com>
    Date: Wed, 23 Dec 2020 16:35:43 +0100
    Subject: [PATCH] fixup! extract_utils: implement patchelf
    
    * Move $PATCHELF exporting from oat2dex to setup_vendor
    * Since it requires $HOST to be set, let's make it global, so oat2dex can also use it
    
    Change-Id: I406f70ef1ce9ec7a0998f77439b7d3fa3ec4e62a
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/tools/extract_utils.sh
+++ build/tools/extract_utils.sh
@@ -30,6 +30,7 @@ ARCHES=
 FULLY_DEODEXED=-1
 
 TMPDIR=$(mktemp -d)
+HOST="$(uname | tr '[:upper:]' '[:lower:]')"
 
 #
 # cleanup
@@ -101,6 +102,10 @@ function setup_vendor() {
     else
         VENDOR_STATE=0
         VENDOR_RADIO_STATE=0
+    fi
+
+    if [ -z "$PATCHELF" ]; then
+        export PATCHELF="$HENTAI_ROOT"/prebuilts/tools-hentai/${HOST}-x86/bin/patchelf
     fi
 }
 
@@ -1245,7 +1250,6 @@ function oat2dex() {
     local SRC="$3"
     local TARGET=
     local OAT=
-    local HOST="$(uname)"
 
     if [ -z "$BAKSMALIJAR" ] || [ -z "$SMALIJAR" ]; then
         export BAKSMALIJAR="$PIXELDUST_ROOT"/prebuilts/tools-custom/common/smali/baksmali.jar
