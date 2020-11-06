8a7533b2d04d8fd8ce5b1dcb28574ccedac2ce5b
Arne Coucheron <arco68@gmail.com>
2020-11-07 00:46:10 +0100

    soong: Add flag for disabling postrender cleanup
    Some legacy devices experiences bad lag unless this is disabled.
    
    Disable it with:
    TARGET_DISABLE_POSTRENDER_CLEANUP := true
    
    Change-Id: I2b40dc4cbcab06fb293d1f407321af2fae846a51
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -29,6 +29,7 @@ SOONG_CONFIG_NAMESPACES += pixeldustGlobalVars
 SOONG_CONFIG_pixeldustGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
@@ -59,6 +60,7 @@ SOONG_CONFIG_pixeldustQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_pixeldustGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_pixeldustGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_pixeldustGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_pixeldustGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
