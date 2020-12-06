669205c3fdcfdde0c39828e88604369a452a343a
spezi77 <spezi7713@gmx.net>
2020-12-06 14:21:47 +0100

    Changelog: Revert latest changes.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -1,67 +1,17 @@
-#!/bin/bash
-# note: re-generating days is possible. Just delete the dates you want regenerated
-# from the top (!!) of the changelog file in $OUT/system/etc/Changelog.txt
-# 2nd line of the file must be the date to fetch from!!
-GREEN="\033[1;32m"
-YELLOW="\033[1;33m"
-NC="\033[0m"
+#!/bin/sh
 
 export Changelog=Changelog.txt
-export PassedDays=30 # change this to limit max changelog days
-export MANIFEST="${TOP}/.repo/manifests/manifests/pixeldust.xml"
 
-if [ -f "${OUT}/system/etc/${Changelog}" ] # if changelog already generated
+if [ -f $Changelog ];
 then
-	cp "${OUT}/system/etc/${Changelog}" $Changelog
-	LastDate=`sed '2!d' $Changelog` # get 2nd line of changelog file
-	LastDate="$(echo -e "${LastDate}" | tr -d '[:space:]')" # get rid of whitespaces
-	# extract month day and year from string:
-	Month=${LastDate:0:2}
-	Day=${LastDate:3:2}
-	Year=${LastDate:6:4}
-	LastDate=`date -d "${Year}${Month}${Day}" +%s`
-	TimeNow=`date +%s` # current time
-	DayDiff=$(( (TimeNow - LastDate) / 86400 )) # n/o days passed
-	if [[ $DayDiff < $PassedDays ]]; then # don't fetch more than max days
-		export PassedDays=$DayDiff
-		mv $Changelog "${Changelog}.bak" # save current changelog for later
-	fi
-	touch $Changelog
-else
-	touch $Changelog
+	rm -f $Changelog
 fi
 
-if [[ $PassedDays == 0 ]]; then
-	echo "Already have today"
-	rm $Changelog
-	rm "${Changelog}.bak"
-	exit 0
-else
-	echo -e "${GREEN}Regenerating log of last ${YELLOW}${PassedDays}${GREEN} days${NC}"
-	if [ -f "${OUT}/system/etc/${Changelog}" ]; then
-		rm "${OUT}/system/etc/${Changelog}"
-		rm "${OUT}/${Changelog}"
-	fi
-fi
+touch $Changelog
 
-# Build a list of all repos
-IFS='
-'
-PROJECTPATHS=$(grep "<project" "${MANIFEST}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
-PROJECTPATHS+='
-'
+echo "Generating changelog..."
 
-# Add repos in local manifest for DT changelog
-for lManifest in $TOP/.repo/local_manifests/*; do
-	PROJECTPATHS+=$(grep "<project" "${lManifest}" | sed -n 's/.*path="\([^"]\+\)".*/\1/p')
-done
-
-echo -e "${GREEN}Generating changelog...${NC}"
-
-# Save current dir
-pDIR=$(echo $PWD)
-
-for i in $(seq $PassedDays);
+for i in $(seq 30);
 do
 export After_Date=`date --date="$i days ago" +%m-%d-%Y`
 k=$(expr $i - 1)
@@ -69,30 +19,16 @@ k=$(expr $i - 1)
 
 	# Line with after --- until was too long for a small ListView
 	echo '====================' >> $Changelog;
-	echo "     "$Until_Date     >> $Changelog;
+	echo  "     "$Until_Date    >> $Changelog;
 	echo '====================' >> $Changelog;
 	echo >> $Changelog;
 
 	# Cycle through every repo to find commits between 2 dates
-	for PROJECTPATH in ${PROJECTPATHS}; do
-		cd "${TOP}/${PROJECTPATH}"
-		if ! [[ -z $(git log --after=$After_Date --until=$Until_Date) ]]; then # only echo if there is a change
-			echo "[${PROJECTPATH}]" >> $pDIR/$Changelog
-			git log --format="%s <%ar> [%h]%nby: %an (%ae)" --after=$After_Date --until=$Until_Date >> $pDIR/$Changelog
-			echo >> $pDIR/$Changelog
-		fi
-	done
-	cd $pDIR
+	repo forall -pc 'git log --oneline --after=$After_Date --until=$Until_Date' >> $Changelog
 	echo >> $Changelog;
 done
 
 sed -i 's/project/   */g' $Changelog
-
-if [ -f "${Changelog}.bak" ]
-then
-	cat "${Changelog}.bak" >> $Changelog # restore old changelog to the end of current one
-	rm "${Changelog}.bak"
-fi
 
 cp $Changelog $OUT/system/etc/
 cp $Changelog $OUT/
