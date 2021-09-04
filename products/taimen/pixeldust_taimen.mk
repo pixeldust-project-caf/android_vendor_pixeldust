0b79296c39cf37e3e0a7893c4785d04e1899fddc
Nitin1438 <nitin.chobhe@gmail.com>
2021-09-04 12:05:23 +0530

    taimen: update make file and remove dependencies for now

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -1,33 +1,9 @@
-# Copyright (C) 2020 The PixelDust Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-#  limitations under the License.
-
 # Boot animation
 BOOTANIMATION := 1440
 
 # Release name
 PRODUCT_RELEASE_NAME := Pixel 2 XL
 export TARGET_DEVICE=taimen
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-
-#
-# All components inherited here go to system_ext image
-#
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 
 # Don't include Google apex use the AOSP variant instead 
 TARGET_EXCLUDE_GOOGLE_APEX := true
@@ -38,16 +14,24 @@ include vendor/pixeldust/configs/pixeldust_phone.mk
 # Include optional stuff (e.g. prebuilt apps)
 include vendor/pixeldust/configs/system_optional.mk
 
-#
-# All components inherited here go to vendor image
-#
-# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
-# Inherit product specific makefiles
-$(call inherit-product, device/google/taimen/device.mk)
-$(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
+-include device/google/taimen/device-lineage.mk
+
+# Device identifier. This must come after all inclusions
+PRODUCT_NAME := pixeldust_taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
+
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
 
 # Google Apps
 $(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
@@ -69,16 +53,8 @@ REMOVE_GAPPS_PACKAGES += \
     pixel_experience_2020 \
     PrebuiltGmail \
     RecorderPrebuilt \
+    SCONE \
     YouTube \
     YouTubeMusicPrebuilt \
     WellbeingPrebuilt \
     talkback
-
-PRODUCT_COPY_FILES += \
-    device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
-
-PRODUCT_MANUFACTURER := Google
-PRODUCT_BRAND := google
-PRODUCT_NAME := pixeldust_taimen
-PRODUCT_DEVICE := taimen
-PRODUCT_MODEL := Pixel 2 XL
