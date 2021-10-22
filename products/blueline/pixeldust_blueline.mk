0158b9e0d85da254931faf6a9826f2c67ee0dd97
spezi77 <spezi7713@gmx.net>
2021-10-22 23:39:47 +0200

    bluecross: Adjust product make files to our new base product.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- products/blueline/pixeldust_blueline.mk
+++ products/blueline/pixeldust_blueline.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2019-2020 The PixelDust Project
+#
+# Copyright (C) 2019-2021 The PixelDust Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,51 +12,69 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+
+#
+# All components inherited here go to system image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
+
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+#
+# All components inherited here go to product image
+#
+$(call inherit-product, vendor/pixeldust/build/product/pixeldust_product.mk)
+
+#
+# All components inherited here go to vendor image
+#
+# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
+
+# Release name
+export TARGET_DEVICE=blueline
 
 # Bootanimation
 BOOTANIMATION := 1080
 
-# Release name
-PRODUCT_RELEASE_NAME := Pixel 3
-export TARGET_DEVICE=blueline
-
-# Use the sepolicies which are being shipped with our device
-TARGET_EXCLUDE_QCOM_VENDOR_SEPOLICY := true
-
-# Opt out from our custom cross-device sepolicy
-TARGET_EXCLUDE_CUSTOM_DEVICE_SEPOLICY := true
-
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, device/google/crosshatch/aosp_blueline.mk)
-
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Include common PixelDust stuff
-include vendor/pixeldust/configs/pixeldust_phone.mk
-
-# Include optional stuff (e.g. prebuilt apps)
-include vendor/pixeldust/configs/system_optional.mk
-
-# Google vendor
-$(call inherit-product-if-exists, vendor/google/blueline/blueline-vendor.mk)
-
 # Google Apps
+WITH_GMS := true
 DEVICE_REQUIRES_CARRIER_APPS := true
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
 REMOVE_GAPPS_PACKAGES += \
     pixel_2016_exclusive \
     pixel_experience_2019_midyear \
     pixel_experience_2019 \
     pixel_experience_2020_midyear \
-    pixel_experience_2020
+    pixel_experience_2020 \
 
+# Product properties
 PRODUCT_NAME := pixeldust_blueline
 PRODUCT_DEVICE := blueline
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 3
+PRODUCT_MANUFACTURER := Google
 
-PRODUCT_PROPERTY_OVERRIDES += \
+# Match stock build fingerprint
+PRODUCT_OVERRIDE_FINGERPRINT := google/blueline/blueline:12/SP1A.210812.015/7679548:user/release-keys
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.pixeldust.maintainer="spezi77" \
     ro.pixeldust.device="blueline"
+
+# Inherit AOSP stuff
+$(call inherit-product, vendor/pixeldust/configs/telephony.mk)
+$(call inherit-product, device/google/crosshatch/device-blueline.mk)
+
+PRODUCT_COPY_FILES += device/google/crosshatch/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
+# b/189477034: Bypass build time check on uses_libs until vendor fixes all their apps
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+$(call inherit-product, vendor/google/blueline/blueline-vendor.mk)
