f6f3ab1651e2e8703a2be3be5da003854ac79ad9
spezi77 <spezi7713@gmx.net>
2021-12-13 17:36:17 +0100

    configs: Add CAF packages to the build. based on: https://github.com/CAF-Extended/device_qcom_common/commit/4b6c8d154c5e433de550a258aa6d6c1742099673
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_caf.mk
+++ configs/pixeldust_caf.mk
@@ -22,3 +22,33 @@ SKIP_BOOT_JARS_CHECK := true
 
 # Include definitions for Snapdragon Clang
 $(call inherit-product, vendor/qcom/sdclang/config/SnapdragonClang.mk)
+
+# QTI VNDK Framework Detect
+PRODUCT_PACKAGES += \
+    libvndfwk_detect_jni.qti \
+    libqti_vndfwk_detect \
+    libvndfwk_detect_jni.qti.vendor \
+    libqti_vndfwk_detect.vendor
+
+# Packages
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    libjson \
+    Stk \
+    tcmiface \
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
+
+PRODUCT_BOOT_JARS += \
+    tcmiface \
+    telephony-ext
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
