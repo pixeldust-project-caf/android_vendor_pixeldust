0a0542c7e3b50529eaeace9b9d88639eaaa0a9e5
spezi77 <spezi7713@gmx.net>
2022-01-17 11:48:30 +0100

    products: Bringup taimen for SaltedCaramel.
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Change-Id: I419142d7d124614536fd1b271d4a6909296d5dc6

--- products/taimen/pixeldust_taimen.mk
+++ products/taimen/pixeldust_taimen.mk
@@ -1,63 +1,116 @@
-# Boot animation
-BOOTANIMATION := 1440
+#
+# Copyright (C) 2019-2022 The PixelDust Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# All components inherited here go to system image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
+
+#
+# All components inherited here go to system_ext image
+#
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+#
+# All components inherited here go to product image
+#
+$(call inherit-product, vendor/pixeldust/build/product/pixeldust_product.mk)
+
+#
+# All components inherited here go to vendor image
+#
+# TODO(b/136525499): move *_vendor.mk into the vendor makefile later
+$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := Pixel 2 XL
 export TARGET_DEVICE=taimen
 
-# Don't include Google apex use the AOSP variant instead 
-TARGET_EXCLUDE_GOOGLE_APEX := true
+# Bootanimation
+BOOTANIMATION := 1440
 
-# Include common PixelDust stuff
-include vendor/pixeldust/configs/pixeldust_phone.mk
-
-# Include optional stuff (e.g. prebuilt apps)
-include vendor/pixeldust/configs/system_optional.mk
-
-# Inherit device configuration
-$(call inherit-product, device/google/taimen/aosp_taimen.mk)
-
--include device/google/taimen/device-lineage.mk
-
-# Device identifier. This must come after all inclusions
-PRODUCT_NAME := pixeldust_taimen
-PRODUCT_BRAND := google
-PRODUCT_MODEL := Pixel 2 XL
-PRODUCT_RESTRICT_VENDOR_FILES := false
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
-
-BUILD_FINGERPRINT := google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
-
-$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+# Google Apex
+WITH_GOOGLE_APEX := true
 
 # Google Apps
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+WITH_GMS := true
+DEVICE_REQUIRES_CARRIER_APPS := true
 REMOVE_GAPPS_PACKAGES += \
-    DevicePersonalizationPrebuiltPixel2020 \
+    CalculatorGooglePrebuilt \
+    CalendarGooglePrebuilt \
+    Chrome \
+    DevicePersonalizationPrebuiltPixel2021 \
     DevicePolicyPrebuilt \
     DiagnosticsToolPrebuilt \
     Drive \
+    DocumentsUIGoogle \
     FilesPrebuilt \
     GCS \
     GoogleTTS \
+    LatinIMEGooglePrebuilt \
     Maps \
-    NetworkPermissionConfigGoogle \
-    NetworkStackGoogle \
     NgaResources \
-    pixel_2016_exclusive \
-    pixel_2018_exclusive.xml \
-    pixel_experience_2018 \
+    Photos \
+    pixel_2018_exclusive \
     pixel_experience_2019_midyear \
     pixel_experience_2019 \
     pixel_experience_2020_midyear \
     pixel_experience_2020 \
+    pixel_experience_2021_midyear \
+    pixel_experience_2021 \
+    PixelWallpapers2021 \
     PrebuiltGmail \
     RecorderPrebuilt \
     SCONE \
+    SoundAmplifierPrebuilt \
+    talkback \
+    TrichromeLibrary \
+    Tycho \
     YouTube \
     YouTubeMusicPrebuilt \
-    WellbeingPrebuilt \
-    talkback
+    WebViewGoogle \
+    WellbeingPrebuilt
+
+# Product properties
+PRODUCT_NAME := pixeldust_taimen
+PRODUCT_DEVICE := taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_MANUFACTURER := Google
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.pixeldust.maintainer="Nitin1438" \
+    ro.pixeldust.device="taimen"
+
+# Inherit AOSP stuff
+$(call inherit-product, vendor/pixeldust/configs/telephony.mk)
+$(call inherit-product, device/google/taimen/device.mk)
+
+PRODUCT_COPY_FILES += \
+    device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
+
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_PACKAGES += \
+    vndk_package \
+    com.android.vndk.current.on_vendor
+
+# b/189477034: Bypass build time check on uses_libs until vendor fixes all their apps
+PRODUCT_BROKEN_VERIFY_USES_LIBRARIES := true
+
+$(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
