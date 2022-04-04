fb1d3537b66889a231046713b8de0d7f6c0df486
LuK1337 <priv.luk@gmail.com>
2022-04-04 21:31:18 +0200

    configs: Disable remote keyguard animation until it's fixed
    Fixes slow post biometric auth screen wakeup.
    
    Change-Id: I8f4824220f14a6fe93e792d15bcaf12fdf2c0ddf
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/pixeldust_phone.mk
+++ configs/pixeldust_phone.mk
@@ -77,6 +77,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.launcher.blur.appLaunch=false
 
+# Disable remote keyguard animation
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.wm.enable_remote_keyguard_animation=0
+
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/pixeldust/overlay
 
