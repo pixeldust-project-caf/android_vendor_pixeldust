add918ef8b862ef4d6f8fb2feb833564ddb88a80
electimon <electimon@gmail.com>
2022-06-18 19:30:24 +0000

    build: qcom: Include qcom_boards.mk
    * Can you believe it was missed?
    
    Signed-off-by: electimon <electimon@gmail.com>
    Change-Id: Id1a4d075de0732d43af8d1107be3913a50a44f98
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -23,4 +23,5 @@ $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
+include $(TOPDIR)vendor/pixeldust/build/core/vendor/qcom_boards.mk
 endif
