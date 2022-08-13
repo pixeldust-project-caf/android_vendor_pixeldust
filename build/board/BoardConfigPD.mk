d73d806ebaab933744a1b7a9b6c697cce23377c8
Anay Wadhera <anay1018@gmail.com>
2022-08-14 03:53:32 +0900

    [android14] config: remove kernel build task
    We will move fully to an AOSP build system with prebuilt kernel
    
    Change-Id: I19a0ab0be1154780aa8b0bf46cdc1272a1efd7e1
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/board/BoardConfigPD.mk
+++ build/board/BoardConfigPD.mk
@@ -3,10 +3,5 @@
 #    SPDX-License-Identifier: Apache-2.0
 #
 
-# Kernel
-ifneq ($(TARGET_NO_KERNEL),true)
-include vendor/pixeldust/build/board/BoardConfigKernel.mk
-endif
-
 # Soong
 include vendor/pixeldust/build/board/BoardConfigSoong.mk
