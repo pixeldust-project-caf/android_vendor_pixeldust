a74cfaefaa1e17ca577b1d6e1d685729cabc3baa
spezi77 <spezi7713@gmx.net>
2022-09-29 15:52:52 +0200

    configs: change to PD AOSP Tiramisu.
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- configs/version.mk
+++ configs/version.mk
@@ -15,9 +15,9 @@
 #
 
 # Pixel Dust ROM versioning
-PD_ROM_MAJ_VERSION := PixelDust-Tartufo
+PD_ROM_MAJ_VERSION := PixelDust-Tiramisu
 ANDROID_OS_VERSION := 13.0.0
-BUILD_VERSION_CODE := caf
+BUILD_VERSION_CODE := aosp
 
 ifndef SIGN_KEY
   PD_BUILDTYPE := UNOFFICIAL
