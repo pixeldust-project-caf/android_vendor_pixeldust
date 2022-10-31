26db1d57ddea1ece7be6ced7991199431a4dd4e3
Michael Bestas <mikeioannina@gmail.com>
2022-11-01 00:49:48 +0100

    config: Remove TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY This hasn't been ported after lineage-18.1
    Change-Id: Ib3ae63ee549b774349268a42923e462a3ef975a0
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -41,8 +41,7 @@ SOONG_CONFIG_pixeldustGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
-    target_uses_prebuilt_dynamic_partitions \
-    uses_camera_parameter_lib
+    target_uses_prebuilt_dynamic_partitions
 
 SOONG_CONFIG_NAMESPACES += pixeldustNvidiaVars
 SOONG_CONFIG_pixeldustNvidiaVars += \
@@ -82,7 +81,6 @@ SOONG_CONFIG_pixeldustQcomVars_needs_camera_boottime_timestamp := $(TARGET_CAMER
 BOOTLOADER_MESSAGE_OFFSET ?= 0
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
 TARGET_INIT_VENDOR_LIB ?= vendor_init
-TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY ?= libcamera_parameters
 TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
@@ -92,7 +90,6 @@ SOONG_CONFIG_pixeldustGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_
 SOONG_CONFIG_pixeldustGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_pixeldustGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
 SOONG_CONFIG_pixeldustGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_FOD_LIB)
-SOONG_CONFIG_pixeldustGlobalVars_uses_camera_parameter_lib := $(TARGET_SPECIFIC_CAMERA_PARAMETER_LIBRARY)
 
 ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
 SOONG_CONFIG_pixeldustQcomVars_qcom_display_headers_namespace := vendor/qcom/opensource/commonsys-intf/display
