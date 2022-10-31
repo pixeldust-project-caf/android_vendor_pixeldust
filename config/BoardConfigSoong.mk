666c56c5c640c8f409387879a20f1d47b0e41d17
Michael Bestas <mikeioannina@gmail.com>
2022-11-01 00:50:46 +0100

    config: Remove TARGET_HAS_MEMFD_BACKPORT
    Change-Id: I17444007429f826bf3d7504cba760204c476e664
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -33,7 +33,6 @@ SOONG_CONFIG_pixeldustGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
     disable_postrender_cleanup \
-    has_memfd_backport \
     ignores_ftp_pptp_conntrack_failure \
     needs_netd_direct_connect_rule \
     target_camera_needs_client_info \
@@ -64,7 +63,6 @@ endif
 
 # Soong bool variables
 SOONG_CONFIG_pixeldustGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
-SOONG_CONFIG_pixeldustGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
 SOONG_CONFIG_pixeldustGlobalVars_ignores_ftp_pptp_conntrack_failure := $(TARGET_IGNORES_FTP_PPTP_CONNTRACK_FAILURE)
 SOONG_CONFIG_pixeldustGlobalVars_needs_netd_direct_connect_rule := $(TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE)
 SOONG_CONFIG_pixeldustGlobalVars_target_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
