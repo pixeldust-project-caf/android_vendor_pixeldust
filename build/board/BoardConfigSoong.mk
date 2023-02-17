Project-1CE/vendor_ice

soong: add config variables for udfps extension
Change-Id: I499b31d00a053b865ceaa3b34a9762f980bbf14e

target/board/BoardConfigSoong.mk
@@ -29,12 +29,15 @@ SOONG_CONFIG_NAMESPACES += iceGlobalVars
 SOONG_CONFIG_iceGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_process_sdk_version_override
+    target_process_sdk_version_override \
+    target_surfaceflinger_udfps_lib
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_iceGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_iceGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_iceGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_iceGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)

pixeldust-project-caf/android_vendor_pixeldust

Signed-off-by: spezi77 <spezi7713@gmx.net>

build/board/BoardConfigSoong.mk
@@ -18,12 +18,16 @@ SOONG_CONFIG_NAMESPACES += pixeldustGlobalVars
 SOONG_CONFIG_pixeldustGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_process_sdk_version_override
+    target_process_sdk_version_override \
+    target_surfaceflinger_udfps_lib
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_pixeldustGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_pixeldustGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_pixeldustGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_pixeldustGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
+
cd2c4ecb3906f1be7c11bd597dd8eb77e3f51b18
TheScarastic <warabhishek@gmail.com>
2021-10-12 06:09:57 +0000

    soong: add config variables for udfps extension
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- build/board/BoardConfigSoong.mk
+++ build/board/BoardConfigSoong.mk
@@ -18,12 +18,16 @@ SOONG_CONFIG_NAMESPACES += pixeldustGlobalVars
 SOONG_CONFIG_pixeldustGlobalVars += \
     target_init_vendor_lib \
     target_ld_shim_libs \
-    target_process_sdk_version_override
+    target_process_sdk_version_override \
+    target_surfaceflinger_udfps_lib
 
 # Set default values
 TARGET_INIT_VENDOR_LIB ?= vendor_init
+TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_pixeldustGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_pixeldustGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
 SOONG_CONFIG_pixeldustGlobalVars_target_process_sdk_version_override := $(TARGET_PROCESS_SDK_VERSION_OVERRIDE)
+SOONG_CONFIG_pixeldustGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
+
