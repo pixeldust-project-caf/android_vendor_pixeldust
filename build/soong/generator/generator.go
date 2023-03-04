PixelExperience/vendor_aosp

vendor: Revert last soong commits
Revert "soong: generator: Replace android.SourceDepTag usage with IsSourceDepTagWithOutputTag"

This reverts commit d370757ea5c05b2476823fb842fdee534645ace8.

Revert "Revert "vendor: soong: Rework soong generator""

This reverts commit 5b830e782599518879cee893ffc0964d9024e854.

Change-Id: I0a9e760d74feae8fad9202b52aeacff397ceb249

build/soong/generator/generator.go
@@ -186,9 +186,7 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 					ctx.ModuleErrorf("host tool %q missing output file", tool)
 				}
 			default:
-				if !android.IsSourceDepTagWithOutputTag(ctx.OtherModuleDependencyTag(module), "") {
-					ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
-				}
+				ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
 			}
 		})
 	}
@@ -229,9 +227,25 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		}
 	}
 
-	cmd := customExpandVariables(ctx, String(g.properties.Cmd))
+	exCmd := customExpandVariables(ctx, String(g.properties.Cmd))
 
-	rawCommand, err := android.Expand(cmd, func(name string) (string, error) {
+	// Dummy output dep
+	dummyDep := android.PathForModuleGen(ctx, ".dummy_dep")
+
+	genDir := android.PathForModuleGen(ctx)
+
+	// Pick a unique rule name and the user-visible description.
+	manifestName := "custom.sbox.textproto"
+	desc := "generate"
+	name := "generator"
+	manifestPath := android.PathForModuleOut(ctx, manifestName)
+
+	// Use a RuleBuilder to create a rule that runs the command inside an sbox sandbox.
+	rule := android.NewRuleBuilder(pctx, ctx).Sbox(genDir, manifestPath).SandboxTools()
+	rule.Command().Text("touch").Output(dummyDep)
+        cmd := rule.Command()
+
+	rawCommand, err := android.Expand(exCmd, func(name string) (string, error) {
 		switch name {
 		case "location":
 			if len(g.properties.Tools) == 0 && len(toolFiles) == 0 {
@@ -263,25 +277,14 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		return
 	}
 
-	// Dummy output dep
-	dummyDep := android.PathForModuleGen(ctx, ".dummy_dep")
-
-	buildDir := android.PathForOutput(ctx, "generator")
-	genDir := android.PathForModuleGen(ctx)
-
-	// Use a RuleBuilder to create a rule that runs the command inside an sbox sandbox.
-	rule := android.NewRuleBuilder(pctx, ctx).Sbox(genDir, buildDir).SandboxTools()
-
-	rule.Command().
-		Text(rawCommand).
-		ImplicitOutput(dummyDep).
-		Implicits(g.inputDeps).
-		Implicits(g.implicitDeps)
-	rule.Command().Text("touch").Output(dummyDep)
+	cmd.Text(rawCommand)
+	cmd.ImplicitOutput(dummyDep)
+	cmd.Implicits(g.inputDeps)
+	cmd.Implicits(g.implicitDeps)
 
 	g.outputDeps = append(g.outputDeps, dummyDep)
 
-	rule.Build("generator", "generate")
+	rule.Build(name, desc)
 }
 
 func NewGenerator() *Module {

pixeldust-project-caf/android_vendor_pixeldust

build/soong/generator/generator.go
@@ -187,9 +187,7 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 					ctx.ModuleErrorf("host tool %q missing output file", tool)
 				}
 			default:
-				if !android.IsSourceDepTagWithOutputTag(ctx.OtherModuleDependencyTag(module), "") {
-					ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
-				}
+				ctx.ModuleErrorf("unknown dependency on %q", ctx.OtherModuleName(module))
 			}
 		})
 	}
@@ -230,9 +228,25 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		}
 	}
 
-	cmd := pixeldustExpandVariables(ctx, String(g.properties.Cmd))
+	exCmd := pixeldustExpandVariables(ctx, String(g.properties.Cmd))
 
-	rawCommand, err := android.Expand(cmd, func(name string) (string, error) {
+	// Dummy output dep
+	dummyDep := android.PathForModuleGen(ctx, ".dummy_dep")
+
+	genDir := android.PathForModuleGen(ctx)
+
+	// Pick a unique rule name and the user-visible description.
+	manifestName := "custom.sbox.textproto"
+	desc := "generate"
+	name := "generator"
+	manifestPath := android.PathForModuleOut(ctx, manifestName)
+
+	// Use a RuleBuilder to create a rule that runs the command inside an sbox sandbox.
+	rule := android.NewRuleBuilder(pctx, ctx).Sbox(genDir, manifestPath).SandboxTools()
+	rule.Command().Text("touch").Output(dummyDep)
+        cmd := rule.Command()
+
+	rawCommand, err := android.Expand(exCmd, func(name string) (string, error) {
 		switch name {
 		case "location":
 			if len(g.properties.Tools) == 0 && len(toolFiles) == 0 {
@@ -264,36 +278,14 @@ func (g *Module) GenerateAndroidBuildActions(ctx android.ModuleContext) {
 		return
 	}
 
-	// Dummy output dep
-	dummyDep := android.PathForModuleGen(ctx, ".dummy_dep")
-
-	// tell the sbox command which directory to use as its sandbox root
-	buildDir := android.PathForOutput(ctx).String()
-	sandboxPath := shared.TempDirForOutDir(buildDir)
-
-	genDir := android.PathForModuleGen(ctx)
-	// Escape the command for the shell
-	rawCommand = "'" + strings.Replace(rawCommand, "'", `'\''`, -1) + "'"
-	sandboxCommand := fmt.Sprintf("$sboxCmd --sandbox-path %s --output-root %s --copy-all-output -c %s && touch %s",
-		sandboxPath, genDir, rawCommand, dummyDep.String())
-
-	ruleParams := blueprint.RuleParams{
-		Command:     sandboxCommand,
-		CommandDeps: []string{"$sboxCmd"},
-	}
-	g.rule = ctx.Rule(pctx, "generator", ruleParams)
-
-	params := android.BuildParams{
-		Rule:        g.rule,
-		Description: "generate",
-		Output:      dummyDep,
-		Inputs:      g.inputDeps,
-		Implicits:   g.implicitDeps,
-	}
+	cmd.Text(rawCommand)
+	cmd.ImplicitOutput(dummyDep)
+	cmd.Implicits(g.inputDeps)
+	cmd.Implicits(g.implicitDeps)
 
 	g.outputDeps = append(g.outputDeps, dummyDep)
 
-	ctx.Build(pctx, params)
+	rule.Build(name, desc)
 }
 
 func NewGenerator() *Module {